69846060d4c66efba40aea977fc85afcfe8ab52e
DandingB <8073687+DandingB@users.noreply.github.com>
2024-05-14 14:57:02 +0200

    Added VS project files

--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,25 @@ public:
 	void OnPaint() override
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//glBegin(GL_TRIANGLES);
+		//glColor3f(1.0f, 0.0f, 0.0f);
+		//glVertex2i(0, 1);
+		//glColor3f(0.0f, 1.0f, 1.0f);
+		//glVertex2i(-1, -1);
+		//glColor3f(0.0f, 1.0f, 1.0f);
+		//glVertex2i(1, -1);
+		//glEnd();
+
 		glBegin(GL_TRIANGLES);
 		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2i(0, 1);
+		glVertex2f(-0.4, 0.4);
 		glColor3f(0.0f, 1.0f, 1.0f);
-		glVertex2i(-1, -1);
+		glVertex2f(-1, -1);
 		glColor3f(0.0f, 1.0f, 1.0f);
-		glVertex2i(1, -1);
+		glVertex2f(1, -1);
 		glEnd();
+
 		glFlush();
 	}
 
--- src/platform/Windows.cpp
+++ src/platform/Windows.cpp
@@ -130,7 +130,6 @@ cxWindowBase::cxWindowBase()
         return;
 
     CreateContext();
-
 }
 
 cxWindowBase::~cxWindowBase()
